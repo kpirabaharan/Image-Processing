b1d6fe6b66d2cd3fc3278acc5101ee55f3758837
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-11-03 18:19:49 -0400

    Section 3: Drawing Circle on OpenCV Image
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ OpenCV-Basics/direct_drawing.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+
+# Fn
+def draw_circle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img, (x, y), 100, (0, 255, 0), -1)
+
+
+cv2.namedWindow(winname="drawing")
+cv2.setMouseCallback("drawing", draw_circle)
+
+# Show Img with OpenCV
+img = np.zeros((512, 512, 3))
+
+while True:
+    cv2.imshow("drawing", img)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
